fdc3f9bfe342851b2a298f163c24a8092a727f18
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-10 10:24:03 +0300

    add App component and MovieCard file

---
+++ src/Pages/Favorites.tsx
@@ -0,0 +1,10 @@
+function Favorites(){
+    return <div className="favorites-empty">
+        <h2>No favorites yet!</h2>
+        <p>Start adding movies...</p>
+
+
+    </div>
+}
+
+export default Favorites;
