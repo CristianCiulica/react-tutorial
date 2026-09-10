bc0012a5d6638d81761f9f9db7b443f006b3a2de
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-10 10:43:54 +0300

    add App component and MovieCard file

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import "./App.css"
 import Home from "./Pages/Home.tsx";
 import {Routes, Route} from "react-router-dom";
 import Favorites from "./Pages/Favorites.tsx";
+import NavBar from "./Components/NavBar.tsx";
+
 function App(){
     return(
+        <div>
+            <NavBar></NavBar>
         <main className="main-content">
            <Routes>
                <Route path="/" element={<Home/>}></Route>
                <Route path="/favorites" element={<Favorites/>}></Route>
            </Routes>
         </main>
+        </div>
     );
 }
 
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,6 +6,11 @@ function NavBar(){
             <Link to="/" >MovieApp</Link>
         </div>
 
+        <div className="navbar-links">
+            <Link to="/" className="nav-link" >Home</Link>
+            <Link to="/favorites" className="nav-link" >Favorites</Link>
+        </div>
+
     </nav>
 }
 
